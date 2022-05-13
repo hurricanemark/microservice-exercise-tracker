86a21f3cc58458702b49dd002d1bac3676b03f6e
Mark V Nguyen <24466116+hurricanemark@users.noreply.github.com>
2022-05-13 02:34:51 +0000

    Format dateString as 'Day, Month date year'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser');
-const moment = require('moment');
+//const moment = require('moment');
 require('dotenv').config()
 let mongoose;
 try {
@@ -18,7 +18,7 @@ try {
 const exerciseSessionSchema = new mongoose.Schema({
   description: { type: String, required: true },
   duration: { type: Number, required: true },
-  date: { type: Date, required: true }
+  date: { type: String, required: true }
 });
 let ExerciseSession = mongoose.model('ExerciseSession', exerciseSessionSchema);
 
@@ -68,15 +68,12 @@ app.get('/api/users', function(req, res) {
 //POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
 app.post('/api/users/:_id/exercises', bodyParser.urlencoded({ extended: false }), function(req, res) {
   let uid = req.params._id
-  console.log('exercises-- uid: ',uid)
-  console.log('\n',req.body);
   
   let newExercise = new ExerciseSession({
     username: req.body.username,
     description: req.body.description,
     duration: parseInt(req.body.duration),
-    date: req.body.date || defaultDate,
-    _id: uid.toString()
+    date: req.body.date || defaultDate
   })
   // in case date filled is empty, assign Now to it
   if (!newExercise.date) {
@@ -88,18 +85,16 @@ app.post('/api/users/:_id/exercises', bodyParser.urlencoded({ extended: false })
   User.findByIdAndUpdate(uid, { $push: { 'log': newExercise } }, { new: true }, function(err, updatedUser) {
     if (err) { console.log(err) }
     else {
-      console.log("UpdatedUSER: ",updatedUser);
-      
       let responseObj = {}
-      responseObj['_id'] = uid;
+      responseObj['_id'] = uid.toString();
       responseObj['username'] = updatedUser.username;
       responseObj['description'] = newExercise.description;
       responseObj['duration'] = newExercise.duration;
-      responseObj['date'] = newExercise.date.toDateString() || new date().toDateString();
+      responseObj['date'] = new Date(newExercise.date).toDateString() || new Date().toDateString();
 
-      console.log('\n----------->');
-      console.log(responseObj);
-      console.log('\n<-----------\n');
+      // console.log('\n----------->');
+      // console.log(responseObj);
+      // console.log('\n<-----------\n');
       
       res.json(responseObj);
     }
@@ -112,9 +107,9 @@ app.post('/api/users/:_id/exercises', bodyParser.urlencoded({ extended: false })
 // GET request to /api/users/:_id/logs to retrieve a full exercise log of any user
 app.get('/api/users/:_id/logs', function(req, res) {
   
-  let uid = req.params._id
-  console.log("uid:", uid);
-  //console.log(uid)
+  let uid = req.params._id;
+  const {from, to, limit} = req.query;
+  
   User.findById(uid, function(err, foundUser) {
     if(!foundUser) return res.json({
       count: 0,
@@ -124,47 +119,62 @@ app.get('/api/users/:_id/logs', function(req, res) {
       return res.json({ error: err})
     }
 
+    // reformat log's date:
+    for (index = 0; index < foundUser.log.length; index++) {
+      // console.log('===============');
+      // console.log('DEBUG(before) -- logDate: ', foundUser.log[index].date);
+      var oridt = foundUser.log[index].date;
+      var ndt = formatDateString(oridt);
+      foundUser.log[index].date = ndt;
+      // console.log('DEBUG(after) formattedLogDate: ', ndt);
+      // console.log('---> ', foundUser.log[index].date);
+
+      // console.log('===============');
+    }
+
+
+    
     let responseObj = foundUser;
-
-    let newSession = new ExerciseSession({
-      description: foundUser.log.description,
-      duration: parseInt(foundUser.log.duration),
-      date: moment(foundUser.log.date).format('YYYY-MM-DD').toString()
-    });
-    
-    
-    if (!newSession.date) {
-      newSession.date = new Date().toDateString();
-    }
-    
-    console.log("NewSession date: ", newSession.date);
-    
-    responseObj['username'] = foundUser.username;
-
-
-
-    if (req.query.from || req.query.to) {
-      let fromDate = new Date().toDateString();
-      let toDate = new Date(0).toDateString();
-      if (req.query.from) {
-        fromDate = new Date(req.query.from).getTime();
-      }
-      if (req.query.to) {
-        toDate = new Date(req.query.to).getTime();
-      }
-      responseObj.log = responseObj.log.filter((ExerciseSession) => {
-        let logDate = new Date(ExerciseSession.date).getTime();
-        responseObj.log.date = newSession.date.toDateString();
-        return logDate >= fromDate && logDate <= toDate;
-      });
-
-      if (req.query.limit){
-        responseObj.log = responseObj.log.slice(0, req.query.limit)
-      }      
-    }
-
     responseObj['count'] = foundUser.log.length;
     
+    // let newSession = new ExerciseSession({
+    //   description: foundUser.log.description,
+    //   duration: parseInt(foundUser.log.duration),
+    //   date: foundUser.log.date
+    // });
+    
+    
+    // if (!newSession.date) {
+    //   newSession.date = new Date().toDateString();
+    // }
+    
+    responseObj['username'] = foundUser.username;
+    
+    
+
+    let fromDate = new Date().toDateString();
+    let toDate = new Date(0).toDateString();
+    if (from) {
+      fromDate = new Date(from).getTime();
+    }
+    if (to) {
+      toDate = new Date(req.query.to).getTime();
+    }  
+    if (from || to) {
+      responseObj.log = responseObj.log.filter((ExerciseSession) => {
+        let logDate = new Date(ExerciseSession.date).getTime();
+        responseObj.log.date = foundUser.log.date;
+          
+        return logDate >= fromDate && logDate <= toDate;
+      });
+      console.log('got from-to: ', from);
+    }
+
+    if (parseFloat(limit) > 0){
+      responseObj.log = responseObj.log.slice(0, parseInt(limit))
+    }  
+    responseObj['count'] = responseObj.log.length;
+
     
     console.log(responseObj);
     res.json(responseObj);
@@ -172,6 +182,72 @@ app.get('/api/users/:_id/logs', function(req, res) {
   })
 });
 
+
+function formatDateString(inDateStr) {
+  let dateStr = new Date(inDateStr);
+  //console.log('DEBUG: inDateStr: ', inDateStr);
+  const days = [
+  'Sun',
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat'
+  ]
+  var dayIndex = dateStr.getDay()
+  var dayName = days[dayIndex] // Thu
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
+  var monthIndex = dateStr.getMonth()
+  var monthName = months[monthIndex]
+  var year = dateStr.getFullYear() // 2019
+  var date = dateStr.getDate() // 23
+
+  return dayName + ', ' + [monthName, date, year].join(' ');
+}
+
+
+app.get('/api/exercise/log', (req, res) => {
+  const {userId, from, to, limit} = req.query;
+  
+  let temp=getExercisesFromUserWithId(userId);
+  
+  if(from){
+    const fromDate= new Date(from)
+    temp = temp.filter(exe => new Date(exe.date) > fromDate);
+  }
+  
+  if(to){
+    const toDate = new Date(to)
+    temp = temp.filter(exe => new Date(exe.date) < toDate);
+  }
+  
+  if(limit){
+    temp = temp.slice(0,limit);
+  }
+  
+  const log = {
+    _id:userId,
+    username:getUsernameById(userId),
+    count:parseFloat(temp.length),
+    log:temp
+  }
+  
+  res.json(log)
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Microservice server is listening on port ' + listener.address().port)
